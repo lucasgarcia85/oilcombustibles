16a39377f97df6f88b5d010fd658e8aabc0074f3
Lucas Garcia Buixens <Lucasgb@hotmail.com>
2024-04-11 19:55:22 -0300

    slider pagination out / back to top/ img size en header

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -16,16 +16,16 @@ const swiper = new Swiper('.swiper', {
   },
   effect: "fade",
   speed: 1000,
-  pagination: {
-    el: '.swiper-pagination',
-  },
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
   
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
 
   // And if we need scrollbar
   scrollbar: {
