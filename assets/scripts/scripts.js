8212ccfe3a17ed55113511907ba5ae393d93804b
Lucas Garcia Buixens <Lucasgb@hotmail.com>
2024-04-12 19:49:27 -0300

    fix whatsapp number y consulta-bar

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -5,6 +5,21 @@ window.addEventListener('scroll', function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+
+/*consulta bar js para que desaparezca al scrolear
+*****************************************************/
+// window.addEventListener('scroll', function() {
+//   var header = document.querySelector('header');
+//   var consultaBar = document.querySelector('.consulta-bar');
+  
+//   if (header.classList.contains('sticky')) {
+//       consultaBar.classList.add('hide');
+     
+//   } else {
+//       consultaBar.classList.remove('hide');
+//   }
+// });
+
 // Swiper config
 const swiper = new Swiper('.swiper', {
   // Optional parameters
